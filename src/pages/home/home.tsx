a2f8b5e04133d1beaa6429330c138530918c1e79
pclevy <pclevybr@gmail.com>
2025-10-08 19:35:54 -0300

    Esphinge - Xadrez de olho no Futuro. 08/10/2025, 19:35

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -88,7 +88,7 @@ function Home() {
         }}
       >
         <h3>Nossos Símbolos, nossa História</h3>
-        <h5>(em construção ... XadrezDeOlhoNoFuturo: 06/10/2025, 19:54)</h5>
+        <h5>(em construção ... XadrezDeOlhoNoFuturo: 08/10/2025, 19:11)</h5>
 
         {/* -- Inicia codigo Contador -- */}
         {/*
