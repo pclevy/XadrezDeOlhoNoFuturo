00b4141ad76da05e4bbbd06e98daf7564100c3bf
pclevy <pclevybr@gmail.com>
2025-12-02 18:40:35 -0300

    Retornando antes 28/11 - 02/12/2025, 18:40

--- src/pages/eventosFuturos/EventosFuturos.tsx
+++ src/pages/eventosFuturos/EventosFuturos.tsx
@@ -106,7 +106,7 @@ function EventosFuturos() {
         }}
       >
         <h3 tabIndex={0}>Eventos Futuros</h3>
-        <h5 tabIndex={0}>(em construção ... 24.10.2025, 21:56)</h5>
+        <h5 tabIndex={0}>(em construção ... 02.12.2025, 18:40)</h5>
         <h4
           tabIndex={0}
           aria-label="Abaixo lista de Eventos, sendo que cada Evento ao receber foco, apresenta ao lado direito o cartaz correspondente"
